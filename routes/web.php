3ae7fa6a881a2b4a71e9899288797b2f6540c3f5
Faza01 <91936066+Faza01@users.noreply.github.com>
2022-04-14 05:39:17 +0700

    front-end

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home', [
+        "title" => "Home"
+    ]);
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "title" => "About"
+    ]);
+});
+
+Route::get('/keranjang', function () {
+    return view('keranjang', [
+        "title" => "Keranjang"
+    ]);
+});
+
+Route::get('/shop_item', function () {
+    return view('shop', [
+        "title" => "Shop"
+    ]);
+});
